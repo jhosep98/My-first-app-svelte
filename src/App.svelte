6bb12b6f08bd3d9180f58924c0eef2ca95763550
jhosep98 <jhosep149@hotmail.com>
2020-09-17 22:59:04 -0300

    Add button

--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,23 @@
     font-weight: 100;
     font-family: "Comic Sans MS", cursive;
   }
-
+  .myButton {
+    background-color: white;
+    color: black;
+    border: 2px solid #f44336;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+  }
+  .myButton:hover {
+    background-color: #f44336;
+    color: white;
+  }
   @media (min-width: 640px) {
     main {
       max-width: none;
@@ -33,7 +49,7 @@
 <main>
   <h1>Hello {name}!</h1>
   <p>My first application using svelte. &#128526</p>
-  <button on:click={handleClick}>
+  <button on:click={handleClick} class="myButton">
     Clicked {count}
     {count === 1 ? 'time' : 'times'}
   </button>
